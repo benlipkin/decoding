4fac4777a448e6dc26b378b7798f9787698ab4b3
benlipkin <bglipkin@gmail.com>
2024-11-18 17:08:48 -0500

    chore: bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ import pathlib
 import setuptools
 
 core_requirements = [
-    "jax==0.4.31",
-    "jaxtyping==0.2.34",
+    "jax==0.4.35",
+    "jaxtyping==0.2.36",
     "vllm==0.6.3.post1",
 ]
 dev_requirements = [
     "pdoc==15.0.0",
     "pre-commit==4.0.1",
-    "pyright==1.1.385",
+    "pyright==1.1.389",
     "pytest==8.3.3",
-    "pytest-cov==5.0.0",
+    "pytest-cov==6.0.0",
     "pytest-html==4.1.1",
-    "ruff==0.7.0",
+    "ruff==0.7.4",
 ]
 
 with pathlib.Path("README.md").open(encoding="utf-8") as f:
@@ -26,7 +26,7 @@ with pathlib.Path("README.md").open(encoding="utf-8") as f:
 
 setuptools.setup(
     name="decoding",
-    version="0.1.2",
+    version="0.1.3",
     description="Composable inference algorithms with LLMs and programmable logic",
     long_description=readme,
     long_description_content_type="text/markdown",
